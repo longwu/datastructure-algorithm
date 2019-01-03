978630fd68ec4e4709571b88955136f31c34652e
Jason Wu <wulong0795@hotmail.com>
2019-01-03 23:55:45 +0800

    优化选择排序代码

--- src/main/java/com/study/sort/SelectSort.java
+++ src/main/java/com/study/sort/SelectSort.java
@@ -1,16 +1,16 @@
 package com.study.sort;
 
-import com.study.tools.Utils;
-
 /**
- * 选择排序,
- * 每次遍历中从所需要排序的元素中选出最小的,放在已排序好的元素后面
+ * 选择排序
+ * <p>
+ * 每次遍历中从所需要排序的元素中选出最小的,放在已排序好的元素最后,直到全部排序结束
+ *
  * <p>
  * https://www.cnblogs.com/shen-hua/p/5424059.html
- *
+ * <p>
  * 算法执行比较次数: sum = (n-1) + (n-2) + .... +1 = n(n-1)/2
- *
- * 时间复杂度 O(n2)
+ * <p>
+ * 时间复杂度O(n^2)
  */
 public class SelectSort {
 
@@ -19,47 +19,47 @@ public class SelectSort {
         int[] a = {12, 23, 9, 24, 15, 3, 18};
 
         selectSort(a);
+        print(a);
+
+        System.out.println();
 
         selectSortDesc(a);
-
-        Utils.print(a);
+        print(a);
     }
 
     /**
      * 选择排序, 从每次遍历未排序的元素中找出值最小的元素索引
      *
-     * @param a
      */
-    public static void selectSort(int[] a) {
-        int n = a.length;
-        for (int k = 0; k < n - 1; k++) {
+    private static void selectSort(int[] a) {
+        //由于最后一个元素无需再遍历了,只要前面的所有元素都是按顺序从小到大排列,那么最后一个就是最大值, 所以只要判断i < a.length-1即可
+        for (int i = 0; i < a.length - 1; i++) {
             //假设最小值索引为min, min初始值为k
-            int min = k;
-            for (int i = k + 1; i < n; i++) {
+            int min = i;
+            for (int j = i + 1; j < a.length; j++) {
                 //如果后面元素的值小于第min个元素,则把min修改为后面元素的索引,循环结束后得出最小的min
-                if (a[i] < a[min]) {
-                    min = i;
+                if (a[j] < a[min]) {
+                    min = j;
                 }
             }
             //将最小的索引为m的元素放到前面k位置上
-            if (k != min) {
-                int temp = a[k];
-                a[k] = a[min];
+            if (i != min) {
+                int temp = a[i];
+                a[i] = a[min];
                 a[min] = temp;
             }
         }
     }
 
     /**
-     * 选择排序, 降序
-     * @param a
+     * 从大到小排序
+     *
      */
-    public static void selectSortDesc(int[] a) {
-        int n = a.length;
-        for (int i = 0; i < n - 1; i++) {
+    private static void selectSortDesc(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
             int max = i;
             //将后面的元素与前面元素做比较,如果后面的大,则把max索引改为最大的元素索引
-            for (int j = i + 1; j < n; j++) {
+            for (int j = i + 1; j < a.length; j++) {
                 if (a[max] < a[j]) {
                     max = j;
                 }
@@ -69,6 +69,16 @@ public class SelectSort {
                 int tmp = a[max];
                 a[max] = a[i];
                 a[i] = tmp;
+            }
+        }
+    }
+
+    private static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                System.out.print(arr[i]);
+            } else {
+                System.out.print(arr[i] + ",");
             }
         }
     }
